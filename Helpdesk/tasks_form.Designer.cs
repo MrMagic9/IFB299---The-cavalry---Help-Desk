4c43e519bdfe2c9f87a9ececc618dd24a50894ba
lachlan96 <lachlan.ashley@connect.qut.edu.au>
2015-09-13 23:58:38 +1000

    Helpdesk

--- Helpdesk/tasks_form.Designer.cs
+++ Helpdesk/tasks_form.Designer.cs
@@ -34,6 +34,9 @@
             this.label2 = new System.Windows.Forms.Label();
             this.clear_button = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,7 +66,7 @@
             this.tasks_checkedListBox.Location = new System.Drawing.Point(45, 80);
             this.tasks_checkedListBox.Name = "tasks_checkedListBox";
             this.tasks_checkedListBox.ScrollAlwaysVisible = true;
-            this.tasks_checkedListBox.Size = new System.Drawing.Size(440, 293);
+            this.tasks_checkedListBox.Size = new System.Drawing.Size(440, 225);
             this.tasks_checkedListBox.TabIndex = 13;
             this.tasks_checkedListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.tasks_checkedListBox_ItemCheck);
             // 
@@ -80,7 +83,7 @@
             // clear_button
             // 
             this.clear_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.clear_button.Location = new System.Drawing.Point(176, 421);
+            this.clear_button.Location = new System.Drawing.Point(279, 421);
             this.clear_button.Name = "clear_button";
             this.clear_button.Size = new System.Drawing.Size(173, 40);
             this.clear_button.TabIndex = 14;
@@ -92,17 +95,51 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(189, 387);
+            this.label4.Location = new System.Drawing.Point(188, 390);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(141, 18);
             this.label4.TabIndex = 16;
             this.label4.Text = "Completed Tasks: 0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(77, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 40);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Add a Task";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(117, 335);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(306, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 40);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
             // 
             // tasks_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(537, 473);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.clear_button);
             this.Controls.Add(this.tasks_checkedListBox);
@@ -126,5 +163,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button clear_button;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
